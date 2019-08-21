86d376356b0930d695728a1f064015512fcac4d3
Denis John <denis.john.ndarake@gmail.com>
2019-08-21 11:26:57 +0100

    Added Post Page to display individual post

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Post from "./components/Post";
 
 export default class App extends Component {
   render() {
@@ -11,9 +12,12 @@ export default class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
